329c88ef96ac057c0d322a1cfbaf0f401d5bbd85
Stephen Brennan <stephen@brennan.io>
2019-10-24 21:52:27 -0400

    Add regex checker for new product page

--- whiskeybot.py
+++ whiskeybot.py
@@ -11,35 +11,55 @@ from notifyme import NotifyMeMailer
 
 SUBJECT = 'WHISKEY ALERT!'
 URLS = {
-    'https://www.klwines.com/Products?searchText=redbreast': {
+    'K&L - Redbreast Product Search': {
+        'url': 'https://www.klwines.com/Products?searchText=redbreast',
         'type': 'xpath',
         'xpath': '//div[contains(@class, "result-desc")]',
         'expected': 6,
     },
-    'https://www.blackwellswines.com/search?q=Redbreast': {
+    'Blackwell - Redbreast Product Search': {
+        'url': 'https://www.blackwellswines.com/search?q=Redbreast',
         'type': 'xpath',
         'xpath': '//div[@class="product_row"]',
         'expected': 6,
-    }
+    },
+    'K&L - New Product Page': {
+        'url': 'https://www.klwines.com/Products?&filters=sv2_NewProductFeedYN$eq$1$True$ProductFeed$!dflt-stock-all!206&limit=50&offset=0&orderBy=LotGeneratedFromPOYN%20asc,NewProductFeedDate%20desc&searchText=',
+        'type': 'regex',
+        'regex': r'(?i)redbreast',
+        'expected': 0,
+    },
 }
 
 
-def xpath(url, response, config):
+def xpath(name, response, config):
+    url = config['url']
     tree = html.fromstring(response.text)
     found = len(tree.xpath(config['xpath']))
     num_expected = config['expected']
     if found != num_expected:
-        yield f'Found {found} xpath results, expected {num_expected} in {url}'
+        yield f'{name}: found {found} xpath results, expected {num_expected} in {url}'
+
+
+def regex(name, response, config):
+    url = config['url']
+    found = len(re.findall(config['regex'], response.text))
+    num_expected = config['expected']
+    if found != num_expected:
+        yield f'{name}: found {found} regex results, expected {num_expected} in {url}'
 
 
 def check(urls):
     mailer = NotifyMeMailer.create()
     messages = []
-    for url, config in urls.items():
-        print(f'Looking in {url}...')
+    for name, config in urls.items():
+        print(f'Looking in {name}...')
+        url = config['url']
         response = requests.get(url)
         if config['type'] == 'xpath':
-            gen = xpath(url, response, config)
+            gen = xpath(name, response, config)
+        elif config['type'] == 'regex':
+            gen = regex(name, response, config)
 
         messages.extend(list(gen))
 
