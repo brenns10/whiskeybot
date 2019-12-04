dea57211d36910d8733484207a9c8126b23ed36c
Stephen Brennan <stephen@brennan.io>
2019-12-03 20:39:58 -0800

    I'm getting annoyed with updating expected values all the time

--- whiskeybot.py
+++ whiskeybot.py
@@ -1,74 +1,103 @@
 # -*- coding: utf-8 -*-
 """
 Check whether Redbreast Small Batch is available at some stores and email if so.
+
+This script expects to be run periodically, probably as a cron job. It keeps a
+state file containing each URL it searches, and the results that it has found.
+If the results it finds don't match the state (e.g. the state file is not
+present, a URL is not present in the file, or the number of results differ from
+the value in the state file), it will send an email and update the state. In
+this way, alerts will be sent when things change but not again.
 """
 import re
+import json
+import os.path
 
 import requests
 from lxml import html
 from notifyme import NotifyMeMailer
 
 
+# Counts will be stored here
+STATEFILE = '~/whiskey.json'
 SUBJECT = 'WHISKEY ALERT!'
 URLS = {
     'K&L - Redbreast Product Search': {
         'url': 'https://www.klwines.com/Products?searchText=redbreast',
         'type': 'xpath',
         'xpath': '//div[contains(@class, "tf-product-description")]',
-        'expected': 6,
     },
     'Blackwell - Redbreast Product Search': {
         'url': 'https://www.blackwellswines.com/search?q=Redbreast',
         'type': 'xpath',
         'xpath': '//div[@class="product_row"]',
-        'expected': 7,
     },
     'K&L - New Product Page': {
         'url': 'https://www.klwines.com/Products?&filters=sv2_NewProductFeedYN$eq$1$True$ProductFeed$!dflt-stock-all!206&limit=50&offset=0&orderBy=LotGeneratedFromPOYN%20asc,NewProductFeedDate%20desc&searchText=',
         'type': 'regex',
         'regex': r'(?i)redbreast',
-        'expected': 0,
     },
 }
 
 
-def xpath(name, response, config):
+def xpath(name, response, config, expected):
     url = config['url']
     tree = html.fromstring(response.text)
     found = len(tree.xpath(config['xpath']))
-    num_expected = config['expected']
-    if found != num_expected:
-        yield f'{name}: found {found} xpath results, expected {num_expected} in {url}'
+    if found != expected:
+        return f'{name}: found {found} xpath results, expected {expected} in {url}', found
+    else:
+        return None, None
 
 
-def regex(name, response, config):
+def regex(name, response, config, expected):
     url = config['url']
     found = len(re.findall(config['regex'], response.text))
-    num_expected = config['expected']
-    if found != num_expected:
-        yield f'{name}: found {found} regex results, expected {num_expected} in {url}'
+    if found != expected:
+        return f'{name}: found {found} regex results, expected {expected} in {url}', found
+    else:
+        return None, None
+
+
+def load_state():
+    fullpath = os.path.expanduser(STATEFILE)
+    if os.path.exists(fullpath):
+        with open(fullpath) as f:
+            return json.load(f)
+    return {}
+
+
+def save_state(state):
+    fullpath = os.path.expanduser(STATEFILE)
+    with open(fullpath, 'w') as f:
+        json.dump(state, f)
 
 
 def check(urls):
     mailer = NotifyMeMailer.create()
     messages = []
+    state = load_state()
     for name, config in urls.items():
         print(f'Looking in {name}...')
         url = config['url']
         response = requests.get(url)
+        expected = state.get(name)
         if config['type'] == 'xpath':
-            gen = xpath(name, response, config)
+            message, expected = xpath(name, response, config, expected)
         elif config['type'] == 'regex':
-            gen = regex(name, response, config)
+            message, expected = regex(name, response, config, expected)
 
-        messages.extend(list(gen))
+        if message:
+            messages.append(message)
+            state[name] = expected
 
     if messages:
         final_msg = f'{SUBJECT}\n\nWe found the following results:\n'
         final_msg += '\n'.join(messages)
         mailer.send(SUBJECT, final_msg)
+        save_state(state)
     else:
-        print('Nothing found :(')
+        print('Nothing has changed :(')
 
 
 if __name__ == '__main__':
@@ -77,3 +106,4 @@ if __name__ == '__main__':
     except Exception:
         mailer = NotifyMeMailer.create()
         mailer.send('WHISKEY ERROR', 'Your bot has crashed, pls fix')
+        raise
